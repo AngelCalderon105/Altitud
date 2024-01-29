c231ab987067359799a7bb763b8fa2caeb761f5c
David Calderon <dabidcalderon45@gmail.com>
2024-01-29 13:46:36 -0800

    fixed suberrors redirectories due to stroke-Width to strokeWidth syntax

--- src/app/HomePage/Home.jsx
+++ src/app/HomePage/Home.jsx
@@ -26,8 +26,8 @@ export default function Home() {
                 <ActionButton text = "Contact Us"/>   
                 </div>
                 
-                <a href="" className=' motion-safe:animate-float md:col-span-4 lg:col-span-12 '><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                <a href="" className=' motion-safe:animate-float md:col-span-4 lg:col-span-12 '><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                         </svg>
                 </a>
             </div>
--- src/app/HomePage/Navbar.jsx
+++ src/app/HomePage/Navbar.jsx
@@ -8,8 +8,8 @@ export default function NavBar({navbarList}) {
                 <Logo textSize = "text-2xl md:text-3xl flex items-center" logoSize='w-9 md:w-14'/>
             </div>
             <div className='absolute top-3.5 left-3 w-8 md:hidden'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
                 </svg>
             </div>
         
