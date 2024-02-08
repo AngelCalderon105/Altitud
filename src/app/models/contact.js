8d55f0f14eb27ad4bc94a33cb54c1d1d1e362bdc
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2024-02-08 15:46:03 -0800

    added model lib and env as well as set up routes

---
+++ src/app/models/contact.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const contactSchema = new Schema({
+    clientName:  {
+        type: String,
+        required:[true,'Name is required'],
+        trim: true,
+        minLength:[2,'Name must be longer than 2 characters'],
+        maxLength:[50, 'Name must be lesser than 50 characters'] 
+    },
+    clientEmail:  {
+        type: String,
+        required:[true,'Email is required'],
+        match: [/^[\w.%+-]+@[\w.-]+\.[A-Za-z]{2,}$/i, "Invalid email address"]
+    },
+    clientDescription:  {
+        type: String,
+        required:[true,'Description is required'],
+        trim: true,
+        minLength:[2,'Description must be longer than 2 characters'],
+        maxLength:[50, 'Description must be lesser than 50 characters'] 
+    },
+    clientTimeline:{
+        type: String,
+        required:[true,'Timeline is required'], 
+    }
+   
+});
+
+const Contact =
+  mongoose.models.Contact || mongoose.model("Contact", contactSchema);
+
+export default Contact;
