2f2d795c1497ee1e905669fea6b84110e324812b
David Calderon <dabidcalderon45@gmail.com>
2024-02-22 20:24:04 -0800

    created mobile navbar transitions and mobile navbar page list

--- src/app/HomePage/Navbar.tsx
+++ src/app/HomePage/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState} from 'react'
 import Logo from '../Components/Logo'
 import ActionButton from '../Components/ActionButton'
 import Link from 'next/link'
@@ -11,34 +13,60 @@ interface NavbarItem {
 interface NavbarItemProps {
     navbarList:NavbarItem[];
 }
+
+
+
 export default function NavBar({navbarList}:NavbarItemProps) {
+
+    const[isOpen,setIsOpen] = useState<boolean>(false);
+   
+    const toggleNav = () => {
+        setIsOpen(!isOpen);
+    }
+    
     return (
-        <nav className='bg-dark-blue md:bg-transparent fixed md:absolute top-0 left-0 w-full z-50 md:flex '>
-            <div className='text-3xl flex justify-center items-center md:justify-start py-4 md:px-5 md:w-60'>
-                <Logo textSize = "text-2xl md:text-3xl flex items-center" logoSize='w-9 md:w-14'/>
+<nav className={`bg-dark-blue md:bg-transparent fixed group md:absolute top-0 left-0 w-full z-50 md:flex ${isOpen ? 'nav-open' : 'nav-closed'}`}>
+    
+     <div className='absolute w-full m-14 '>
+        <ul className=' items-center text-lg'>
+                    {navbarList.map((item) => (
+                        <li key={item.name} onClick={toggleNav}>
+                            <Link href={`/${item.path}`}>
+                                <NavbarItem itemName={item.name} />
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+     </div>
+     
+            <div className= {`text-3xl  flex justify-center items-end md:justify-start py-4 md:px-5 md:w-60 h-full`}>
+                <Logo textSize="text-2xl md:text-3xl flex items-center" logoSize='w-9 md:w-14' />
             </div>
-            <div className='absolute top-3.5 left-3 w-8 md:hidden'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-                </svg>
+            <div className='absolute top-3.5 left-3 w-8 md:hidden' onClick={toggleNav}>
+                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="" stroke="currentColor" className="hamburger">
+                   <path className="top-line" strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5" />
+                  <path className="middle-line" strokeLinecap="round" strokeLinejoin="round" d="M3.75 12h16.5" />
+                  <path className="bottom-line" strokeLinecap="round" strokeLinejoin="round" d="M3.75 18.75h16.5" />
+                 </svg>
             </div>
-        
-            <div className='hidden md:flex md:justify-between md:w-full'>
+
+
+            <div className={`hidden md:flex md:justify-between md:w-full ${isOpen ? 'flex' : ''}`}>
                 <ul className='flex items-center text-lg'>
                     {navbarList.map((item) => (
                         <li key={item.name}>
-                            <Link href= {`/${item.path}`}>
-                             <NavbarItem itemName = {item.name}/>
+                            <Link href={`/${item.path}`}>
+                                <NavbarItem itemName={item.name} />
                             </Link>
                         </li>
                     ))}
                 </ul>
                 <div className='px-4 md:flex md:items-center'>
                     <Link href={"/#contact-section"}>
-                    <ActionButton text="Let's Connect" />
+                        <ActionButton text="Let's Connect" />
                     </Link>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
--- src/app/HomePage/NavbarItem.tsx
+++ src/app/HomePage/NavbarItem.tsx
@@ -3,6 +3,6 @@ interface NavbarItemProps {
   }
   
   export default function NavbarItem({ itemName }: NavbarItemProps) {
-    return (<p className="text-xl px-4">{itemName}</p>);
+    return (<p className="text-3xl py-2 md:text-xl md:px-4">{itemName}</p>);
   }
 //we should probably delete this, no longer in use
