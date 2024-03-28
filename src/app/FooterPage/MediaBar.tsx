978bf71de56cbe5fca232929e78966fb6b0952a1
MiguelCalderon2006 <miguel070307@gmail.com>
2024-03-27 21:54:57 -0700

    i added links to socils in the foooter 3-27-24

--- src/app/FooterPage/MediaBar.tsx
+++ src/app/FooterPage/MediaBar.tsx
@@ -1,16 +1,16 @@
-import LinkedInSvg from '../assets/LinkedIn.svg'
-import Tiktok from '../assets/Tiktok.svg'
-import Twitter from '../assets/Twitter.svg'
-import Instagram from '../assets/Instagram.svg'
-import MediaIcon from './MediaIcon'
-export default function MediaBar() {
+import LinkedInSvg from '../assets/LinkedIn.svg';
+import TiktokSvg from '../assets/Tiktok.svg';
+import TwitterSvg from '../assets/Twitter.svg';
+import InstagramSvg from '../assets/Instagram.svg';
+import MediaIcon from './MediaIcon';
 
+export default function MediaBar() {
     return (
-     <div className='flex items-center gap-x-2 lg:gap-x-4'>
-     <MediaIcon icon ={LinkedInSvg}/>
-     <MediaIcon icon ={Tiktok}/>
-     <MediaIcon icon ={Twitter}/>
-     <MediaIcon icon ={Instagram}/>
-     </div>   
-    )
+        <div className='flex items-center gap-x-2 lg:gap-x-4'>
+            <MediaIcon icon={LinkedInSvg} url="https://www.linkedin.com/company/altitud-initiative" />
+            <MediaIcon icon={TiktokSvg} url="https://www.tiktok.com/@altitud.official" />
+            <MediaIcon icon={TwitterSvg} url="https://twitter.com/AltitudOfficial" />
+            <MediaIcon icon={InstagramSvg} url="https://www.instagram.com/altitud.official/" />
+        </div>   
+    );
 }
--- src/app/FooterPage/MediaIcon.tsx
+++ src/app/FooterPage/MediaIcon.tsx
@@ -1,15 +1,14 @@
-import Image from "next/image"
+import Image from "next/image";
 
 interface MediaIconProps {
-    icon:string;
+    icon: string;
+    url: string; // Adding a url prop
 }
-export default function MediaIcon({icon}:MediaIconProps) {
 
+export default function MediaIcon({icon, url}: MediaIconProps) {
     return (
-        <>
-        <a href="/" className="">
+        <a href={url} className="" target="_blank" rel="noopener noreferrer">
             <Image src={icon} alt="" />
         </a>
-        </>
-    )
+    );
 }
